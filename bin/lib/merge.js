84c1f0955e1d1fcf2b5e9abc00856ffdbfc34466
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-07 12:49:45 +0200

    Cleaned merge

--- bin/lib/merge.js
+++ bin/lib/merge.js
@@ -1,12 +1,11 @@
-const fileUtils = require('./utils/fileUtils.js')
+const fileUtils = require('./utils/FileUtils.js')
 
 function mergeFiles(files){
     var data = [];
     
     for(filePath of files){
         var file = fileUtils.getFile(filePath);
-        Array.prototype.push.apply(data,file); 
-        counter ++;
+        Array.prototype.push.apply(data,file);
     }
     
     fileUtils.writeObjectToFile(data, 'merged.json', 4)
