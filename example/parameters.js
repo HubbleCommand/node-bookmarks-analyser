dfd6f70fda5accd7c279d09fcde4541b8afb124a
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-07 12:45:57 +0200

    Examples

---
+++ example/parameters.js
@@ -0,0 +1,19 @@
+//THIS SHOULD BE A JSON, kept as JS to add comments
+parameters = {
+    //Parameters for a specific site
+    "bbc.com" : [
+        {
+            "description": "avb",       //The name of the filter (to identify what you want to do with it)
+            "selector":"xyz",     //The selector
+            "action":"COPY",            //What action to do with the selected elements
+            "options":{
+                
+            }
+        }
+    ],
+
+    //Options, mostly to do if site isn't listed here
+    "options":{
+
+    }
+}
