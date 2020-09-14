73a9631093595813425a13b042b511d95145fa16
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2020-09-14 19:32:19 +0200

    Post functionnality started

--- bin/lib/post.js
+++ bin/lib/post.js
@@ -1,21 +1,37 @@
 const fileUtils = require('./utils/FileUtils.js')
+const { Client } = require('@elastic/elasticsearch')
 
 /**
  * Posts the given object to an ES instance
- * @param {Object} object 
- * @param {URL} url 
+ * @param {Object} object       the object to send
+ * @param {URL} url             the URL of the Elasticsearch instance
+ * @param {String} username     the user of the ES instance
+ * @param {String} password     the password for the user
  */
-function postObject(object, url){
+function postObject(object, url, username, password){
     
 }
 
 /**
  * Posts the given file to an ES instance
- * @param {String} file 
- * @param {URL} url 
+ * @param {String} file         the file to send
+ * @param {URL} url             the URL of the Elasticsearch instance
+ * @param {String} username     the user of the ES instance
+ * @param {String} password     the password for the user
  */
-function postFile(file, url){
+function postFile(file, url, username, password){
+    //Read file
+    var data = fileUtils.getFile(file);
 
+    
+    const client = new Client({ 
+        node: url, 
+        auth:{
+            username:username,
+            password:password
+        } 
+    })
+    client.bulk(data)
 }
 
 exports.postObject = postObject;
--- bin/lib/utils/ESUtils.js
+++ bin/lib/utils/ESUtils.js
@@ -9,4 +9,13 @@ function formatES(object){
 
 }
 
+/**
+ * Verifies that the object is valid
+ * @param {Object} object to check
+ */
+function checkIfValidES(object){
+
+}
+
+exports.checkIfValidES = checkIfValidES;
 exports.formatES = formatES;
